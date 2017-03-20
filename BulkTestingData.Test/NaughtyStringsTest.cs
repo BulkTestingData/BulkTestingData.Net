fe443d2e8605c5627b8ab368a767b12f2cd3faed
Llewellyn Falco <isidore@setgame.com>
2017-03-20 06:55:03 -0700

    about to restart

---
+++ BulkTestingData.Test/NaughtyStringsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using ApprovalUtilities.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BulkTestingData
+{
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class NaughtyStringsTest
+    {
+        [TestMethod]
+        public void TestAll()
+        {
+           Approvals.VerifyAll("length", NaughtyStrings.All12(), s => "{0} => {1}".FormatWith(s, s.Length));
+        }
+    }
+}
